e90aa5c4e9b9c60c3bba0d057b727b38c2935507
Amgalan Byambajargal <amgalan.byambajargal@gmail.com>
2021-11-22 10:28:49 +0100

    chore: add targets

---
+++ __RayTracerKit/__RayTracerKit.h
@@ -0,0 +1,18 @@
+//
+//  __RayTracerKit.h
+//  __RayTracerKit
+//
+//  Created by Amgalan Byambajargal on 22.11.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for __RayTracerKit.
+FOUNDATION_EXPORT double __RayTracerKitVersionNumber;
+
+//! Project version string for __RayTracerKit.
+FOUNDATION_EXPORT const unsigned char __RayTracerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <__RayTracerKit/PublicHeader.h>
+
+
---
+++ __RayTracerKitTests/__RayTracerKitTests.swift
@@ -0,0 +1,4 @@
+import XCTest
+
+final class __RayTracerKitTests: XCTestCase {
+}
