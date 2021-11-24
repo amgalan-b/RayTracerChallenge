1968a11474c283ad8f6383e88d9ed00202ea2309
Amgalan Byambajargal <amgalan.byambajargal@gmail.com>
2021-11-24 11:32:24 +0100

    feat: add light

---
+++ __RayTracerKit/Light.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+struct Light {
+
+    let position: Tuple
+    let intensity: Color
+
+    private init(position: Tuple, intensity: Color) {
+        self.position = position
+        self.intensity = intensity
+    }
+}
+
+extension Light {
+
+    static func point(position: Tuple, intensity: Color) -> Light {
+        return Light(position: position, intensity: intensity)
+    }
+}
+
+#if TEST
+import XCTest
+
+final class LightTests: XCTestCase {
+}
+#endif
