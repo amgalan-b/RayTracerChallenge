0be185da9dd6ecced9c9dd0f1061edae44e24e35
Amgalan Byambajargal <amgalan.byambajargal@gmail.com>
2021-12-10 12:33:05 +0100

    refactor: generalize color

--- __RayTracerKit/Pattern+StripePattern.swift
+++ __RayTracerKit/Pattern+StripePattern.swift
@@ -8,34 +8,24 @@ extension Pattern {
     }
 }
 
-private struct _StripePattern {
+private struct _StripePattern: _Pattern {
 
     let a: Color
     let b: Color
     let transform: Matrix
 
-    public init(_ a: Color, _ b: Color, transform: Matrix = .identity) {
+    init(_ a: Color, _ b: Color, transform: Matrix = .identity) {
         self.a = a
         self.b = b
         self.transform = transform
     }
 
-    fileprivate func _color(at localPoint: Tuple) -> Color {
+    func color(at localPoint: Tuple) -> Color {
         if localPoint.x.floor().truncatingRemainder(dividingBy: 2) == 0 {
             return a
         }
 
         return b
-    }
-}
-
-extension _StripePattern: _Pattern {
-
-    func color(at worldPoint: Tuple, objectTransform: Matrix = .identity) -> Color {
-        let objectPoint = objectTransform.inversed() * worldPoint
-        let patternPoint = transform.inversed() * objectPoint
-
-        return _color(at: patternPoint)
     }
 }
 
@@ -46,42 +36,29 @@ extension PatternTests {
 
     func test_color_constantInY() {
         let pattern = _StripePattern(.white, .black)
-        XCTAssertEqual(pattern._color(at: .point(0, 0, 0)), .white)
-        XCTAssertEqual(pattern._color(at: .point(0, 1, 0)), .white)
-        XCTAssertEqual(pattern._color(at: .point(0, 2, 0)), .white)
+
+        XCTAssertEqual(pattern.color(at: .point(0, 0, 0)), .white)
+        XCTAssertEqual(pattern.color(at: .point(0, 1, 0)), .white)
+        XCTAssertEqual(pattern.color(at: .point(0, 2, 0)), .white)
     }
 
     func test_color_constantInZ() {
         let pattern = _StripePattern(.white, .black)
-        XCTAssertEqual(pattern._color(at: .point(0, 0, 0)), .white)
-        XCTAssertEqual(pattern._color(at: .point(0, 0, 1)), .white)
-        XCTAssertEqual(pattern._color(at: .point(0, 0, 2)), .white)
+
+        XCTAssertEqual(pattern.color(at: .point(0, 0, 0)), .white)
+        XCTAssertEqual(pattern.color(at: .point(0, 0, 1)), .white)
+        XCTAssertEqual(pattern.color(at: .point(0, 0, 2)), .white)
     }
 
     func test_color_alternatesInX() {
         let pattern = _StripePattern(.white, .black)
 
-        XCTAssertEqual(pattern._color(at: .point(0, 0, 0)), .white)
-        XCTAssertEqual(pattern._color(at: .point(0.9, 0, 0)), .white)
-        XCTAssertEqual(pattern._color(at: .point(1, 0, 0)), .black)
-        XCTAssertEqual(pattern._color(at: .point(-0.1, 0, 0)), .black)
-        XCTAssertEqual(pattern._color(at: .point(-1, 0, 0)), .black)
-        XCTAssertEqual(pattern._color(at: .point(-1.1, 0, 0)), .white)
-    }
-
-    func test_objectTransformation() {
-        let pattern = _StripePattern(.white, .black)
-        XCTAssertEqual(pattern.color(at: .point(1.5, 0, 0), objectTransform: .scaling(2, 2, 2)), .white)
-    }
-
-    func test_patternTransformation() {
-        let pattern = _StripePattern(.white, .black, transform: .scaling(2, 2, 2))
-        XCTAssertEqual(pattern.color(at: .point(1.5, 0, 0)), .white)
-    }
-
-    func test_objectAndPatternTransformation() {
-        let pattern = _StripePattern(.white, .black, transform: .translation(0.5, 0, 0))
-        XCTAssertEqual(pattern.color(at: .point(2.5, 0, 0), objectTransform: .scaling(2, 2, 2)), .white)
+        XCTAssertEqual(pattern.color(at: .point(0, 0, 0)), .white)
+        XCTAssertEqual(pattern.color(at: .point(0.9, 0, 0)), .white)
+        XCTAssertEqual(pattern.color(at: .point(1, 0, 0)), .black)
+        XCTAssertEqual(pattern.color(at: .point(-0.1, 0, 0)), .black)
+        XCTAssertEqual(pattern.color(at: .point(-1, 0, 0)), .black)
+        XCTAssertEqual(pattern.color(at: .point(-1.1, 0, 0)), .white)
     }
 }
 #endif
--- __RayTracerKit/Pattern.swift
+++ __RayTracerKit/Pattern.swift
@@ -8,19 +8,39 @@ public struct Pattern {
         _pattern = pattern
     }
 
-    func color(at worldPoint: Tuple, objectTransform: Matrix) -> Color {
-        return _pattern.color(at: worldPoint, objectTransform: objectTransform)
+    func color(at worldPoint: Tuple, objectTransform: Matrix = .identity) -> Color {
+        let objectPoint = objectTransform.inversed() * worldPoint
+        let patternPoint = _pattern.transform.inversed() * objectPoint
+
+        return _pattern.color(at: patternPoint)
     }
 }
 
 protocol _Pattern {
 
-    func color(at worldPoint: Tuple, objectTransform: Matrix) -> Color
+    var transform: Matrix { get }
+
+    func color(at localPoint: Tuple) -> Color
 }
 
 #if TEST
 import XCTest
 
 final class PatternTests: XCTestCase {
+
+    func test_objectTransformation() {
+        let pattern = Pattern.stripe(.white, .black)
+        XCTAssertEqual(pattern.color(at: .point(1.5, 0, 0), objectTransform: .scaling(2, 2, 2)), .white)
+    }
+
+    func test_patternTransformation() {
+        let pattern = Pattern.stripe(.white, .black, .scaling(2, 2, 2))
+        XCTAssertEqual(pattern.color(at: .point(1.5, 0, 0)), .white)
+    }
+
+    func test_objectAndPatternTransformation() {
+        let pattern = Pattern.stripe(.white, .black, .translation(0.5, 0, 0))
+        XCTAssertEqual(pattern.color(at: .point(2.5, 0, 0), objectTransform: .scaling(2, 2, 2)), .white)
+    }
 }
 #endif
