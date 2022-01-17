75f17961d4b0c197e968c1455132df9008b5d09d
Amgalan Byambajargal <amgalan.byambajargal@gmail.com>
2022-01-17 11:48:58 +0100

    feat: bounding box contains

--- __RayTracerKit/BoundingBox.swift
+++ __RayTracerKit/BoundingBox.swift
@@ -13,6 +13,16 @@ final class BoundingBox {
         self.maximum = maximum
     }
 
+    func contains(_ point: Tuple) -> Bool {
+        return (minimum.x ... maximum.x).contains(point.x)
+            && (minimum.y ... maximum.y).contains(point.y)
+            && (minimum.z ... maximum.z).contains(point.z)
+    }
+
+    func contains(_ otherBox: BoundingBox) -> Bool {
+        return contains(otherBox.minimum) && contains(otherBox.maximum)
+    }
+
     func addPoint(_ point: Tuple) {
         minimum.x = min(point.x, minimum.x)
         minimum.y = min(point.y, minimum.y)
@@ -21,6 +31,11 @@ final class BoundingBox {
         maximum.x = max(point.x, maximum.x)
         maximum.y = max(point.y, maximum.y)
         maximum.z = max(point.z, maximum.z)
+    }
+
+    func merge(with otherBox: BoundingBox) {
+        addPoint(otherBox.minimum)
+        addPoint(otherBox.maximum)
     }
 }
 
@@ -41,6 +56,43 @@ final class BoundingBoxTests: XCTestCase {
 
         XCTAssertEqual(box.minimum, .point(-5, 0, -3))
         XCTAssertEqual(box.maximum, .point(7, 2, 0))
+    }
+
+    func test_merge() {
+        let b1 = BoundingBox(minimum: .point(-5, -2, 0), maximum: .point(7, 4, 4))
+        let b2 = BoundingBox(minimum: .point(8, -7, -2), maximum: .point(14, 2, 8))
+        b1.merge(with: b2)
+
+        XCTAssertEqual(b1.minimum, .point(-5, -7, -2))
+        XCTAssertEqual(b1.maximum, .point(14, 4, 8))
+    }
+
+    func test_contains() {
+        let box = BoundingBox(minimum: .point(5, -2, 0), maximum: .point(11, 4, 7))
+
+        XCTAssert(box.contains(.point(5, -2, 0)))
+        XCTAssert(box.contains(.point(11, 4, 7)))
+        XCTAssert(box.contains(.point(8, 1, 3)))
+        XCTAssertFalse(box.contains(.point(3, 0, 3)))
+        XCTAssertFalse(box.contains(.point(8, -4, 3)))
+        XCTAssertFalse(box.contains(.point(8, 1, -1)))
+        XCTAssertFalse(box.contains(.point(13, 1, 3)))
+        XCTAssertFalse(box.contains(.point(8, 5, 3)))
+        XCTAssertFalse(box.contains(.point(8, 1, 8)))
+    }
+
+    func test_contains_box() {
+        let b1 = BoundingBox(minimum: .point(5, -2, 0), maximum: .point(11, 4, 7))
+
+        let b2 = BoundingBox(minimum: .point(5, -2, 0), maximum: .point(11, 4, 7))
+        let b3 = BoundingBox(minimum: .point(6, -1, 1), maximum: .point(10, 3, 6))
+        let b4 = BoundingBox(minimum: .point(4, -3, -1), maximum: .point(10, 3, 6))
+        let b5 = BoundingBox(minimum: .point(6, -1, 1), maximum: .point(12, 5, 8))
+
+        XCTAssert(b1.contains(b2))
+        XCTAssert(b1.contains(b3))
+        XCTAssertFalse(b1.contains(b4))
+        XCTAssertFalse(b1.contains(b5))
     }
 }
 #endif
