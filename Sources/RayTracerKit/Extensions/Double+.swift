912bf5ceb7292a5b2a491b994ad8829d5833e192
Amgalan Byambajargal <amgalan.byambajargal@gmail.com>
2022-02-11 12:47:23 +0100

    feat: modulo

--- Sources/RayTracerKit/Extensions/Double+.swift
+++ Sources/RayTracerKit/Extensions/Double+.swift
@@ -3,4 +3,27 @@ import Foundation
 extension Double {
 
     static let tolerance = 0.00001
+
+    func modulo(dividingBy rhs: Double) -> Double {
+        let remainder = truncatingRemainder(dividingBy: rhs)
+        guard remainder >= 0 else {
+            return remainder + rhs
+        }
+
+        return remainder
+    }
 }
+
+#if TEST
+import XCTest
+
+final class DoubleTests: XCTestCase {
+
+    func test_remainder() {
+        XCTAssertEqual((-0.25).truncatingRemainder(dividingBy: 1), -0.25)
+        XCTAssertEqual((-0.25).remainder(dividingBy: 1), -0.25)
+        XCTAssertEqual(fmod(-0.25, 1), -0.25)
+        XCTAssertEqual((-0.25).modulo(dividingBy: 1), 0.75)
+    }
+}
+#endif
